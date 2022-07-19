0dea323bdd1aec743237f7002a0962be1ec4e685
mghOxfordThesis <109471602+mghOxfordThesis@users.noreply.github.com>
2022-07-19 09:16:29 +0100

    Add files via upload

--- MVSE/R/data.R
+++ MVSE/R/data.R
@@ -1,6 +1,7 @@
 #' Example climate data
 #' 
-#' Daily temperature (degrees Celcius), relative humidity (%) and precipitation (mm) data from 1981 in 2019 in the city of Feira de Santana in Bahia, Brazil.
+#' Daily temperature (degrees Celcius), relative humidity (%) and precipitation (mm) data from 01-01-2015 in 01-01-2017 
+#' in the city of Feira de Santana in Bahia, Brazil.
 #' 
 #' @format A data frame with 468 rows 
 #' \describe{
